ac863b9fe5d32c4256c53515726d6dd1365c110a
Ankit Mishra <ankitmlive@gmail.com>
2020-12-07 12:28:16 +0530

    Create main.py
    Initial Commit

---
+++ main.py
@@ -0,0 +1,25 @@
+"""""""""""""""""""""""""""""""""""""
+""" Leap Year Check in Python """""""
+"""""""""""""""""""""""""""""""""""""
+
+year = 1992
+def is_leap(year):
+    leap = False
+    if year%4 is 0:
+        if year%100 is 0:
+            if year%400 is 0:
+                leap = True
+            else:
+            	leap = False
+        else:
+        	leap = True
+    else:
+        leap = False
+    return leap
+    
+x = is_leap(year)
+print(x)
+
+"""""""""""""""""""""""""""""""""""""""""
+"""" List comprehensions """"""""""""""""
+"""""""""""""""""""""""""""""""""""""""""
