789879b50748fcc2bc609126005a9be74c4cf506
Ankit Mishra <ankitmlive@gmail.com>
2020-12-07 13:27:10 +0530

    Update main.py
    List example 2

--- main.py
+++ main.py
@@ -43,3 +43,16 @@ x = [cube(x) for x in xList]
 print(x)
 
 # Result : [1, 8, 27, 64, 125, 216, 343, 512, 729, 1000]
+
+## EXAMPLE - 2
+
+print([[x, y] for x in [1, 2, 3] for y in [4, 5, 6]])
+# This code is equvalent to :
+results = []
+for x in [1, 2, 3]:
+    for y in [4, 5, 6]:
+        results.append([x, y])
+
+print(results)
+
+Result : [[1, 4], [1, 5], [1, 6], [2, 4], [2, 5], [2, 6], [3, 4], [3, 5], [3, 6]] -- for both case
